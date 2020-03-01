88c237cc83125fdf43a295fb73af8f1745c23677
Lalatendu das <lalatendu.das@intel.com>
2020-03-02 01:21:07 +0530

    GIT: This is GIT experiment to show the usage
    1. pull from remote branch in github
    2. add files and ignore certain file by GIT using .gitignore
    3. Stage a file to be commited
    4. commit the chnages
    5. Push the changes to remote branch of yours.
    
    Signed-off-by: Lalatendu das <lalatendu.das@intel.com>

---
+++ segrigate.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import shutil
+import csv
+import os
+
+def move_file(src, dst):
+    shutil.copy(src, dst)
+
+def read_csv():
+    f = open("./Train.csv", 'r')
+    reader = csv.reader(f)
+    headers = next(reader, None)
+    column = {}
+    for h in range(1,17):
+        column[str(h)] = []
+    for row in reader:
+        column[row[1]].append(row[0])
+    return column
+
+def segrigate(final_dict):
+    os.mkdir("./segri_dir")
+    for dir_name in final_dict.keys():
+        os.mkdir("./segri_dir/" + dir_name)
+    print("Done cereating dirs")
+    for cat_num in final_dict.keys():
+        for file_name in final_dict[cat_num]:
+            move_file("./train/"+file_name, "./segri_dir/" + cat_num)
+
+if __name__ == '__main__':
+    final_dict = {}
+    final_dict = read_csv()
+    print("I am done with population")
+    segrigate(final_dict)
+    print("Done with populatting file in dir")
